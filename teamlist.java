ea9822124e38363aa6059104ab0d816a46a3d4fb
Andrew Schueler <andrew.schueler@hotmail.com>
2017-10-27 18:37:48 -0400

    Fixed more swag

--- teamlist.java
+++ teamlist.java
@@ -2,8 +2,7 @@ public class teamlist
 {
    public static void main(String[] args)
    {
-     System.out.println("This is Benjamin");
-     System.out.println("This is willis");
+     System.out.println("This is Willis");
      System.out.println("This is William");
      System.out.println("This is Parmenion");
      System.out.println("This is Talal");
