985a9fa7290152be141490e4fd4c0dc1c233ca64
Andrew Schueler <andrew.schueler@hotmail.com>
2017-10-26 22:33:56 -0400

    Fix static

--- teamlist.java
+++ teamlist.java
@@ -1,15 +1,13 @@
 public class Hello_teamf 
 {
-   public staic void main(String[] args) 
+   public static void main(String[] args) 
    {
      System.out.println("This is willis");
      System.out.println("This is steph");
      System.out.println("This is William");
-     System.out.println("This is Parmenion");
      System.out.println("This is Talal");
      System.out.println("This is Parmenion");
      System.out.println("This is Benjamin");
      System.out.println("This si Andrew");
-     System.out.println("This is Griffin");
    }
 }
