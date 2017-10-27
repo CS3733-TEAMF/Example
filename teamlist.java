e9b9e9823e75717ac18333c8641e9e7cce93dcc0
gyroth <griffinyr@yahoo.com>
2017-10-26 22:29:04 -0400

    Giffin fixing stuff

--- teamlist.java
+++ teamlist.java
@@ -3,21 +3,13 @@ public class Hello_teamf
    public staic void main(String[] args) 
    {
      System.out.println("This is willis");
- bk_srracca
      System.out.println("This is steph");
-
- bk_Will
-bk_Will
      System.out.println("This is William");
-
      System.out.println("This is Parmenion");
- master
-
      System.out.println("This is Talal");
      System.out.println("This is Parmenion");
      System.out.println("This is Benjamin");
      System.out.println("This si Andrew");
- master
- master
+     System.out.println("This is Griffin");
    }
 }
