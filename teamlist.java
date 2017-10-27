9b922dc388ce0da4e67349face51057be34cfb36
Andrew Schueler <andrew.schueler@hotmail.com>
2017-10-26 23:08:07 -0400

    add Binam

--- teamlist.java
+++ teamlist.java
@@ -1,10 +1,15 @@
-public class Hello_teamf 
+public class teamlist
 {
-   public staic void main(String[] args) 
+   public static void main(String[] args)
    {
+     System.out.println("This is Benjamin");
      System.out.println("This is willis");
-     System.out.println("This is Talal");
+     System.out.println("This is William");
      System.out.println("This is Parmenion");
+     System.out.println("This is Talal");
+     System.out.println("This is Benjamin");
      System.out.println("This is Andrew");
+     System.out.println("This is Griffin");
+     System.out.println("This is Binam");
    }
 }
