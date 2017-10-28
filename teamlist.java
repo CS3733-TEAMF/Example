35b88f20761a8492ca1fdf31098ca75c354a77fe
Andrew Schueler <andrew.schueler@hotmail.com>
2017-10-28 17:07:41 -0400

    fix andrew bro

---
+++ teamlist.java
@@ -0,0 +1,17 @@
+public class teamlist
+{
+   public static void main(String[] args)
+   {
+     System.out.println("This is Willis");
+     System.out.println("This is William");
+     System.out.println("This is Parmenion");
+     System.out.println("This is Talal");
+     System.out.println("This is Benjamin");
+     System.out.println("This is Andrew");
+     System.out.println("This is Griffin");
+     System.out.println("This is Binam");
+     System.out.println("This is Steph");
+     System.out.println("This is Nikos");
+     System.out.println("This is Luke");
+   }
+}
